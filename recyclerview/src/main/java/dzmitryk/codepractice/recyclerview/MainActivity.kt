b6af33a11312bdb5713afd23ebdfe51f3ab01a92
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2025-10-20 22:06:15 +0200

    - Init subproject

---
+++ recyclerview/src/main/java/dzmitryk/codepractice/recyclerview/MainActivity.kt
@@ -0,0 +1,18 @@
+package dzmitryk.codepractice.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dzmitryk.codepractice.recyclerview.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ recyclerview/src/main/java/dzmitryk/codepractice/recyclerview/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package dzmitryk.codepractice.recyclerview.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
