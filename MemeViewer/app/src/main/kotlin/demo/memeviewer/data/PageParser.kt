67cb89bf1e64cef308711f242f5bd0fc381a73f6
Dzmitry_Kalianchuk <dzmitry_kalianchuk@epam.com>
2025-10-20 15:22:21 +0200

    MemeView App added

--- MemeViewer/app/src/main/kotlin/demo/memeviewer/data/PageParser.kt
+++ MemeViewer/app/src/main/kotlin/demo/memeviewer/data/PageParser.kt
@@ -22,15 +22,57 @@ internal class PageParser {
     }
 
     private fun parseElement(element: Element): MemeData? {
-       val imageWrapper = element.selectFirst("div.base-img-wrap-wrap div.base-img-wrap") ?: return null
+        val t = element.selectFirst("h2.base-unit-title a")
+
+        val link = t?.attr("href")?.let { "http://imgflip.com$it" }.orEmpty()
+        val title = t?.text().orEmpty()
+
+        val baseInfo = element.selectFirst("div.base-info") ?: return null
+
+        val nsfwFlag = baseInfo.selectFirst("div.base-flag")
+        if (nsfwFlag != null) {
+            return MemeData.NSFW
+        }
+
+        val author = baseInfo.selectFirst("div.base-author")?.text().orEmpty()
+        val viewCount = baseInfo.selectFirst("div.base-view-count")?.text().orEmpty()
+
+        val imageWrapper = element.selectFirst("div.base-img-wrap-wrap div.base-img-wrap") ?: return null
 
         val nsfw = imageWrapper.selectFirst("div.base-nsfw-msg")
         if (nsfw != null) {
             return MemeData.NSFW
         }
 
-        val imageLink = imageWrapper.selectFirst("a.base-img-link") ?: return null
+        val imageElement = imageWrapper.selectFirst("a.base-img-link")?.child(0) ?: return null
 
+        return if (imageElement.tagName() == "video") {
+            val gifUrl = imageElement.attr("data-src")
+            val posterUrl = imageElement.attr("poster")
 
+            val videoUrl = imageElement.selectFirst("source")?.attr("src").orEmpty()
+
+            return MemeData.Video(
+                title = title,
+                creator = author,
+                viewCount = viewCount,
+                link = link,
+                gifUrl = "https:$gifUrl",
+                videoUrl = "https:$videoUrl",
+                posterUrl = "https:$posterUrl"
+            )
+        } else if (imageElement.tagName() == "img") {
+            val imageUrl = imageElement.attr("src")
+
+            MemeData.Image(
+                title = title,
+                creator = author,
+                viewCount = viewCount,
+                link = link,
+                imageUrl = "https:$imageUrl"
+            )
+        } else {
+            null
+        }
     }
 }
--- MemeViewer/app/src/test/kotlin/demo/memeviewer/data/PageParserTest.kt
+++ MemeViewer/app/src/test/kotlin/demo/memeviewer/data/PageParserTest.kt
@@ -1,5 +1,6 @@
 package demo.memeviewer.data
 
+import demo.memeviewer.model.MemeData
 import demo.memeviewer.model.PageData
 import org.assertj.core.api.Assertions.assertThat
 import org.jsoup.Jsoup
@@ -11,7 +12,45 @@ class PageParserTest {
 
     private val expectedPageData = PageData(
         nextPageUrl = "/m/fun?sort=latest&after=a8kswa",
-        memes = emptyList()
+        memes = listOf(
+            MemeData.Image(
+                title = "sarcasticat",
+                creator = "by kayrobin",
+                link = "http://imgflip.com/i/a8kxw1",
+                viewCount = "8 views" ,
+                imageUrl = "https://i.imgflip.com/a8kxw1.jpg"
+            ),
+            MemeData.Image(
+                title = "Why Do you always wear that mask?",
+                creator = "by misteriosoguayff",
+                link = "http://imgflip.com/i/a8l17f",
+                viewCount = "8 views",
+                imageUrl = "https://i.imgflip.com/a8l17f.jpg"
+            ),
+            MemeData.Image(
+                title = "Tuesday time!!",
+                creator = "by CoolguyMcSwaggy",
+                link = "http://imgflip.com/i/a8kznx",
+                viewCount = "19 views",
+                imageUrl = "https://i.imgflip.com/a8kznx.jpg"
+            ),
+            MemeData.Image(
+                title = "One big threat!",
+                creator = "by CoolguyMcSwaggy",
+                link = "http://imgflip.com/i/a8kz4e",
+                viewCount = "24 views",
+                imageUrl = "https://i.imgflip.com/a8kz4e.jpg"
+            ),
+            MemeData.Image(
+                title = "good vs evil",
+                creator = "by Mememaster10230303",
+                link = "http://imgflip.com/i/a8kz1h",
+                viewCount = "12 views",
+                imageUrl = "https://i.imgflip.com/a8kz1h.jpg"
+            ),
+            MemeData.NSFW,
+            MemeData.NSFW
+        )
     )
 
     @Test
