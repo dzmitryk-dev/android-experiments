c0efc7df079a080b5d72f925368135b1a122fc9b
Dzmitry_Kalianchuk <dzmitry_kalianchuk@epam.com>
2025-10-20 17:58:05 +0200

    MemePagingSource added

--- MemeViewer/app/src/main/kotlin/demo/memeviewer/data/DataSource.kt
+++ MemeViewer/app/src/main/kotlin/demo/memeviewer/data/DataSource.kt
@@ -2,7 +2,9 @@ package demo.memeviewer.data
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.io.IOException
 import javax.inject.Inject
+import kotlin.jvm.Throws
 
 internal class NetworkSource @Inject constructor() : DataSource {
 
@@ -12,5 +14,7 @@ internal class NetworkSource @Inject constructor() : DataSource {
 }
 
 internal interface DataSource {
+
+    @Throws(IOException::class)
     fun get(url: String): Document
 }
---
+++ MemeViewer/app/src/main/kotlin/demo/memeviewer/data/MemePagingSource.kt
@@ -0,0 +1,44 @@
+package demo.memeviewer.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import demo.memeviewer.model.MemeData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+private const val START_URL = "https://imgflip.com/m/fun?sort=latest"
+
+internal class MemePagingSource @Inject constructor(
+    private val dataSource: DataSource,
+    private val parser: PageParser,
+    private val coroutineDispatcher: CoroutineDispatcher,
+) : PagingSource<String, MemeData>() {
+
+    override fun getRefreshKey(state: PagingState<String, MemeData>): String? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.nextKey
+        }
+    }
+
+    override suspend fun load(params: LoadParams<String>): LoadResult<String, MemeData> {
+        val link = params.key ?: START_URL
+
+        return try {
+            val (memes, nextPageUrl) = withContext(coroutineDispatcher) {
+                val page = dataSource.get(link)
+                parser.parse(page).let { result ->
+                    result.copy(memes = result.memes.filterNot { it is MemeData.NSFW } )
+                }
+            }
+
+            LoadResult.Page(
+                data = memes,
+                prevKey = null,
+                nextKey = nextPageUrl,
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
