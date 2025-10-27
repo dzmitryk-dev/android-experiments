656fe6f873a9cf811e36f89adea213f344244834
Dzmitry_Kalianchuk <dzmitry_kalianchuk@epam.com>
2025-10-27 17:37:10 +0100

    Implement by example

--- networkdiscoverydemo/src/main/java/network/discovery/demo/MainActivity.kt
+++ networkdiscoverydemo/src/main/java/network/discovery/demo/MainActivity.kt
@@ -1,47 +1,90 @@
 package network.discovery.demo
 
+import android.content.Context
+import android.net.nsd.NsdManager
+import android.net.nsd.NsdServiceInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import network.discovery.demo.ui.screen.MainScreen
 import network.discovery.demo.ui.theme.AndroidexperimentsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val nsdManager by lazy {
+        getSystemService(Context.NSD_SERVICE) as NsdManager
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             AndroidexperimentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
+
+        lifecycle.addObserver(nsdLifecycleObserver)
+    }
+
+    private val nsdLifecycleObserver = object : DefaultLifecycleObserver {
+
+        override fun onStart(owner: LifecycleOwner) {
+            super.onStart(owner)
+            startServiceDiscovery()
+        }
+
+        override fun onStop(owner: LifecycleOwner) {
+            super.onStop(owner)
+            stopServiceDiscovery()
+        }
+    }
+
+    private fun startServiceDiscovery() {
+        nsdManager.discoverServices(
+//            "_workstation._tcp.",
+            "_ipps._tcp.",
+            NsdManager.PROTOCOL_DNS_SD,
+            nsdDiscoveryListener
+        )
+    }
+
+    private fun stopServiceDiscovery() {
+        nsdManager.stopServiceDiscovery(nsdDiscoveryListener)
+    }
+
+    private val nsdDiscoveryListener = object : NsdManager.DiscoveryListener {
+
+        override fun onDiscoveryStarted(serviceType: String?) {
+            Log.d("DiscoveryListener", "Service discovery started $serviceType")
+        }
+
+        override fun onDiscoveryStopped(serviceType: String?) {
+            Log.d("DiscoveryListener", "Service discovery stopped $serviceType")
+        }
+
+        override fun onServiceFound(serviceInfo: NsdServiceInfo?) {
+            Log.d("DiscoveryListener", "Service discovery found $serviceInfo")
+        }
+
+        override fun onServiceLost(serviceInfo: NsdServiceInfo?) {
+            Log.d("DiscoveryListener", "Service discovery lost $serviceInfo")
+        }
+
+        override fun onStartDiscoveryFailed(serviceType: String?, errorCode: Int) {
+            Log.e("DiscoveryListener", "Service discovery start failed $serviceType with error code $errorCode")
+        }
+
+        override fun onStopDiscoveryFailed(serviceType: String?, errorCode: Int) {
+            Log.e("DiscoveryListener", "Service discovery stop failed $serviceType with error code $errorCode")
+        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidexperimentsTheme {
-        Greeting("Android")
-    }
-}
---
+++ networkdiscoverydemo/src/main/java/network/discovery/demo/ui/screen/MainScreen.kt
@@ -0,0 +1,36 @@
+package network.discovery.demo.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import network.discovery.demo.ui.theme.AndroidexperimentsTheme
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidexperimentsTheme {
+        Greeting("Android")
+    }
+}
